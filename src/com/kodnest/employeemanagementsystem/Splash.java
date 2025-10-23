93133e73167ea5dbc4bc7ef0fc96431960b9e513
maruthign <maruthigngowdaa@gmail.com>
2025-10-23 22:31:13 +0530

    Created First Page

---
+++ src/com/kodnest/employeemanagementsystem/Splash.java
@@ -0,0 +1,39 @@
+package com.kodnest.employeemanagementsystem;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Splash extends JFrame {
+    Splash() {
+
+        getContentPane().setBackground(Color.white);
+        setLayout(null);
+
+        JLabel heading = new JLabel("Employee Management System");
+        heading.setBounds(80, 30, 1200, 60);
+        heading.setFont(new Font("Times New Roman", Font.PLAIN, 60));
+        heading.setForeground(Color.red);
+        add(heading);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/front.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1100, 700, Image.SCALE_SMOOTH);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(50, 100, 1050, 500);
+        add(image);
+
+        JButton clickhere = new JButton("CLICK HERE TO CONTINUE");
+        clickhere.setBounds(400, 400, 300, 70);
+        clickhere.setBackground(Color.WHITE);
+        clickhere.setForeground(Color.BLACK);
+        image.add(clickhere);
+
+        setSize(1170, 650);
+        setLocation(200, 50);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Splash();
+    }
+}
